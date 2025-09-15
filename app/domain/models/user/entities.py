acf1a63e91808741c7b838646f159e9cabb68491
4vertak <4tvert@gmail.com>
2025-09-16 08:02:10 +0900

    Update entities.py

--- app/domain/models/user/entities.py
+++ app/domain/models/user/entities.py
@@ -12,6 +12,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    role = db.Column(db.String(50), nullable=False)  # Doctor, Admin, Patient
+    role = db.Column(db.String(50), nullable=False)  # Doctor, Admin, Disinfector
+
 
     applications = db.relationship("Application", back_populates="user")
