069d7bdb34c9ae161de4f9506a77f61365ddc04b
4vertak <4tvert@gmail.com>
2025-09-16 09:43:50 +0900

    Update entities.py

--- app/domain/models/user/entities.py
+++ app/domain/models/user/entities.py
@@ -1,12 +1,12 @@
 from ....core.extensions import db, login_manager
-
+from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +16,4 @@ class User(db.Model):
 
 
     applications = db.relationship("Application", back_populates="user")
+
