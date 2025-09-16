6ea50759be2bc966beac4483b099e6ffcc1bd9bb
4vertak <4tvert@gmail.com>
2025-09-16 14:47:24 +0900

    Update forms.py

--- app/routes/user/forms.py
+++ app/routes/user/forms.py
@@ -1,39 +1,31 @@
-
-
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, SelectField, StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Optional
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Логин', validators=[
-        DataRequired(message='Логин обязателен'),
-        Length(min=2, max=20, message='Логин должен содержать минимум 2 символа')
-    ])
-    password = PasswordField('Пароль', validators=[
-        DataRequired(message='Пароль обязателен'),
-        Length(min=8, message='Пароль должен содержать минимум 8 символов.')
-    ])
-    confirm_password = PasswordField('Подтвердите пароль', validators=[
-        DataRequired(message='Подтверждение пароля обязательно'),
-        EqualTo('password', message='Пароли не совпадают')
-    ])
-    role = SelectField('Роль', choices=[
-        ('Doctor', 'Врач'),
-        ('Disinfector', 'Дезинфектор'),
-        ('Admin', 'Администратор')
-    ], default='Disinfector')
+    name = StringField('Имя', validators=[DataRequired(
+        message='Имя обязательно'), Length(min=1, max=100)])
+    login = StringField('Логин', validators=[DataRequired(message='Логин обязателен'), Length(
+        min=2, max=20, message='Логин  должен содержать минимум 2 символа')])
+    password = PasswordField('Пароль', validators=[DataRequired(message='Пароль обязателен'), Length(
+        min=8, message='Пароль должен содержать минимум 8 символов.')])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(
+        message='Подтверждение пароля обязательно'), EqualTo('password', message='Пароли не совпадают')])
+    user_type = SelectField('Тип пользователя', choices=[(
+        'doctor', 'Врач'), ('disinfector', 'Дезинфектор'), ('admin', 'Администратор')], default='disinfector')
+    area_id = SelectField('Участок', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Зарегистрироваться')
 
-
 class LoginUpdateForm(FlaskForm):
-    username = StringField('Логин', validators=[
-        DataRequired(message='Логин обязателен'),
-        Length(min=2, max=20)
-    ])
-
+    name = StringField('Имя', validators=[DataRequired(message='Имя обязательно'), 
+                                         Length(min=1, max=100)])
+    login = StringField('Логин', validators=[DataRequired(message='Логин обязателен'), 
+                                           Length(min=2, max=20)])
+    
+    
     change_password = BooleanField('Изменить пароль')
     current_password = PasswordField('Текущий пароль', validators=[Optional()])
-
+    
     new_password = PasswordField('Новый пароль', validators=[
         Optional(),
         Length(min=8, message='Пароль должен содержать минимум 8 символов.')
@@ -42,17 +34,18 @@ class LoginUpdateForm(FlaskForm):
         Optional(),
         EqualTo('new_password', message='Пароли не совпадают')
     ])
-
-    role = SelectField('Роль', choices=[
-        ('Doctor', 'Врач'),
-        ('Disinfector', 'Дезинфектор'),
-        ('Admin', 'Администратор')
-    ], default='Disinfector')
-
+    
+    user_type = SelectField('Тип пользователя', choices=[
+        ('doctor', 'Врач'), 
+        ('disinfector', 'Дезинфектор'), 
+        ('admin', 'Администратор')
+    ], default='disinfector')
+    
+    area_id = SelectField('Участок', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Обновить')
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()])
+    login = StringField('Имя пользователя', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Войти')
