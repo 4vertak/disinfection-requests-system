766b76a1cfbd81bb55b870b54e0a8795abe8fa24
4vertak <4tvert@gmail.com>
2025-09-17 11:04:17 +0900

    Create filters.py - localtime_filter

---
+++ app/utils/filters.py
@@ -0,0 +1,6 @@
+from zoneinfo import ZoneInfo
+
+def localtime_filter(dt, tz_name="Asia/Yakutsk"):
+  if dt is None:
+    return ""
+  return (dt.replace(tzinfo=ZoneInfo("UTC")).astimezone(ZoneInfo(tz_name)).strftime("%d-%m-%Y %H:%M))
